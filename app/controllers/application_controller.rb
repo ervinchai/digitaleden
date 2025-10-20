2247b2fd321b150ae000f11b28155ed15d18bacc
Ervin Chai <ervin.chai1108@gmail.com>
2025-10-20 19:38:10 +0800

    chore: prepare devise auth, user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  inertia_share do
+    {
+      flash: {
+        notice: flash.notice,
+        alert: flash.alert,
+      },
+    }
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  # You must set the APPLICATION_HOST environment variable in production.
+  config.action_mailer.default_url_options = { host: ENV.fetch("APPLICATION_HOST") }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
